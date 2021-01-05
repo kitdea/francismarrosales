329e42f948b862bd2515c5a120ad146e94e9694e
kitdea <francismarrosales@gmail.com>
2021-01-05 20:20:33 +0800

    removed unecessary files

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-const menuBtn = document.getElementsByClassName('toggle-btn')[0];
-const navMenu = document.getElementsByClassName('fm-nav-menu')[0];
+// const menuBtn = document.getElementsByClassName('toggle-btn')[0];
+// const navMenu = document.getElementsByClassName('fm-nav-menu')[0];
 
-menuBtn.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-});
+// menuBtn.addEventListener('click', () => {
+//     navMenu.classList.toggle('active');
+// });
 
+$('.toggle-btn').click(function(){
+    $('.fm-nav-menu').slideToggle(180);
+  });
