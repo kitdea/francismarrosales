95a36e18de21681916f8ee08c5a7c57a26f66e69
kitdea <francismarrosales@gmail.com>
2021-03-14 05:47:55 +0800

    removed unnecessary files and fixed menu not showing in desktop after using in mobile

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-// const menuBtn = document.getElementsByClassName('toggle-btn')[0];
-// const navMenu = document.getElementsByClassName('fm-nav-menu')[0];
-
-// menuBtn.addEventListener('click', () => {
-//     navMenu.classList.toggle('active');
-// });
-
 $('.toggle-btn').click(function(){
     $('.fm-nav-menu').slideToggle(180);
   });
